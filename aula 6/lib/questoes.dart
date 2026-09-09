491135c230d345139f284602df24718c7e1dfc12
Guilherme Pessoa Rossi <163908218+Guilherme-Rossi@users.noreply.github.com>
2026-09-09 20:21:45 -0300

    Add files via upload

---
+++ aula 6/lib/questoes.dart
@@ -0,0 +1,20 @@
+import 'package:tabuada/pergunta.dart';
+
+const questoes = [
+  Pergunta(
+    'Qual organela celular é responsável pela produção de energia (respiração celular)?',
+    ['Mitocôndria', 'Ribossomo', 'Complexo de Golgi', 'Lisossomo'],
+  ),
+  Pergunta(
+    'Qual é o processo pelo qual as plantas produzem seu próprio alimento utilizando a luz solar?',
+    ['Fotossíntese', 'Respiração', 'Fermentação', 'Transpiração'],
+  ),
+  Pergunta(
+    'Em que ano ocorreu a proclamação da independência do Brasil?',
+    ['1822', '1500', '1889', '1808'],
+  ),
+  Pergunta(
+    'Qual civilização antiga construiu as pirâmides de Gizé?',
+    ['Egípcia', 'Grega', 'Romana', 'Maia'],
+  ),
+];
